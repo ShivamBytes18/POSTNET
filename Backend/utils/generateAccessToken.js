edb3f68eb1b648ddd006e6d10eef62d5d7fee323
ShivamBytes18 <su9555181178@gmail.com>
2026-06-05 15:52:24 +0530

    Tokens Creation

---
+++ Backend/utils/generateAccessToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (userId) => {
+    return jwt.sign(
+        { userId },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "15m",
+        }
+    );
+};
+
+export default generateAccessToken;
