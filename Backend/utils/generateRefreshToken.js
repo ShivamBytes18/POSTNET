2e0039d556fdb6af77d0442998954cc1f45ff3ac
ShivamBytes18 <su9555181178@gmail.com>
2026-06-05 15:52:57 +0530

    Refresh Tokens Creation

---
+++ Backend/utils/generateRefreshToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+const generateRefreshToken = (userId) => {
+    return jwt.sign(
+        { userId },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
+};
+
+export default generateRefreshToken;
