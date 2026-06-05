0bef4d0d9e93877696cb0e3850c3b692e5c1df94
ShivamBytes18 <su9555181178@gmail.com>
2026-06-05 12:28:35 +0530

    Post  model Creation

---
+++ Backend/models/post.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    caption: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+
+    videoUrl: {
+      type: String,
+      default: "",
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Post", postSchema);
