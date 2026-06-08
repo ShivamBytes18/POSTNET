6767fbd7b77dfbf41e9d2ec5f867060a78e38728
ShivamBytes18 <su9555181178@gmail.com>
2026-06-08 12:41:12 +0530

    Deployment Issue

--- Backend/index.js
+++ Backend/index.js
@@ -14,10 +14,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
+// app.use(
+//   cors({
+//     origin:["http://localhost:5173",
+//              "https://postnet-dkvceko0d-shiv230104031-8883s-projects.vercel.app",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin:["http://localhost:5173",
-             "https://postnet-dkvceko0d-shiv230104031-8883s-projects.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://postnet-bay.vercel.app",
     ],
     credentials: true,
   })
