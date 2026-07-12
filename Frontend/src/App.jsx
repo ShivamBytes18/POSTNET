88864ed20b0adf767b92bf6c9a4232f9333d5ecb
ShivamBytes18 <su9555181178@gmail.com>
2026-07-12 23:46:22 +0530

    Home Page Directory

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,51 @@
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+// import {
+//   Routes,
+//   Route,
+//   Navigate,
+// } from "react-router-dom";
 
-//import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+// //import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import Home from "./pages/Home";
+
+// function App() {
+//   const token = localStorage.getItem("token");
+
+//   return (
+//     <Routes>
+
+//       <Route
+//         path="/signup"
+//         element={<Signup />}
+//       />
+
+//       <Route
+//         path="/"
+//         element={
+//           token ? (
+//             <Home />
+//           ) : (
+//             <Navigate to="/signup" />
+//           )
+//         }
+//       />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 function App() {
-  const token = localStorage.getItem("token");
-
   return (
     <Routes>
-
-      <Route
-        path="/signup"
-        element={<Signup />}
-      />
-
-      <Route
-        path="/"
-        element={
-          token ? (
-            <Home />
-          ) : (
-            <Navigate to="/signup" />
-          )
-        }
-      />
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
