33f902f0fd46a569bd03152084293ee8d8449a3e
ShivamBytes18 <su9555181178@gmail.com>
2026-07-12 23:39:18 +0530

    Home Page Directory

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import Login from "./pages/Login";
+//import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 
